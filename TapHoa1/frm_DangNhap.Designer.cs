78ae2cea63a5beb9831d4974d739628c7c38d3b5
phanduc111 <phanxuanduc@dtu.edu.vn>
2024-12-14 07:13:39 +0700

    Add project files.

---
+++ TapHoa1/frm_DangNhap.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TapHoa1
+{
+    partial class frm_DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_DangNhap = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_MatKhau = new System.Windows.Forms.TextBox();
+            this.txt_TenNguoiDung = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_DangNhap
+            // 
+            this.btn_DangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DangNhap.Location = new System.Drawing.Point(38, 146);
+            this.btn_DangNhap.Name = "btn_DangNhap";
+            this.btn_DangNhap.Size = new System.Drawing.Size(89, 23);
+            this.btn_DangNhap.TabIndex = 4;
+            this.btn_DangNhap.Text = "Đăng nhập";
+            this.btn_DangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.Location = new System.Drawing.Point(150, 146);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(89, 23);
+            this.btn_Thoat.TabIndex = 6;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_MatKhau);
+            this.groupBox1.Controls.Add(this.txt_TenNguoiDung);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin đăng nhập";
+            // 
+            // txt_MatKhau
+            // 
+            this.txt_MatKhau.Location = new System.Drawing.Point(121, 62);
+            this.txt_MatKhau.Name = "txt_MatKhau";
+            this.txt_MatKhau.PasswordChar = '•';
+            this.txt_MatKhau.Size = new System.Drawing.Size(124, 20);
+            this.txt_MatKhau.TabIndex = 1;
+            // 
+            // txt_TenNguoiDung
+            // 
+            this.txt_TenNguoiDung.Location = new System.Drawing.Point(121, 31);
+            this.txt_TenNguoiDung.Name = "txt_TenNguoiDung";
+            this.txt_TenNguoiDung.Size = new System.Drawing.Size(124, 20);
+            this.txt_TenNguoiDung.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên tài khoản:";
+            // 
+            // frm_DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 215);
+            this.Controls.Add(this.btn_DangNhap);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frm_DangNhap";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_DangNhap;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_MatKhau;
+        private System.Windows.Forms.TextBox txt_TenNguoiDung;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
